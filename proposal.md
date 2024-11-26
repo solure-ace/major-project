62f9d57aaf89515c115f2194a9b88b9abcd3b6eb
solure-ace <av.lynne.w@gmail.com>
2024-11-26 14:12:19 -0600

    Major Projects - SingleGrid class -  now displays gird in center - proposal update

--- proposal.md
+++ proposal.md
@@ -1,25 +1,26 @@
 # Project Description
 
-A game where you go though a level, with multiple goalpoints (eg. you have to  
-get to x many locations before level completion)
+For each level of the game you go though multiple rooms and fighting enemies to find the goalpoints, 
+you need to find certain amount of goal points to complete each level. 
 
 ## Needs to Have List
 
 - Enemies
-- melee & projectile type attacks
-- Generated levels
-- Non-Level area
+- melee & projectile type attacks for the player and enemies
+- randomly generated level layouts made of grids
+- hardcoded "base" area that you can return to
 - 8 directional movement
-- Camera follows players
-- Instructions
+- Start screen with Instructions
+- background music and sound effects
 
 ## Nice to Have List
 
 - Tutorial level
-- Multiple types of levels
-- obstacles that aren't enemies
+- Multiple types of levels (aesthetic)
+- obstacles that aren't enemies eg. walls, pushable objects
 - 8 directional sprites
 - Dialouge system
+- Camera that follows players
 
 
 
--- sketch.js
+++ sketch.js
@@ -5,6 +5,35 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let gameState = "start";
+let you;
+
+//make grid into a class because i will need to make lots of them of different sizes.
+// v grid for class
+let gridOne; 
+
+//grid that works rn ( not an object)
+let grid;
+let cols = 4;
+let rows = 6;
+const CELL_SIZE = 100;
+let gridWidth = cols*CELL_SIZE;
+let gridHeight = rows*CELL_SIZE;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // cant use you.width and you. height because they dont exist yet...???
+
+  // you = new Player(gridWidth/2-15, gridHeight/2-40);
+
+  you = new Player(width/2-15, height/2 - 40);
+
+  grid =  generateEmptyGrid(cols, rows);
+
+  // gridOne = new SingleGrid(6, 4);
+  // gridOne.generateGrid();
+}
+
 class Player {
   constructor(x, y) {
     this.x = x;
@@ -69,34 +98,58 @@ class Player {
       this.x -= this.speed-sqrt(2)/2;
     }
   }
+
+  // stayWithinGrid() {
+  //   if (this.x < ) {
+  //   }
+  // }
 }
 
-let gameState = "start";
-let you;
-let grid; 
-let cols;
-let rows;
-const CELL_SIZE = 100;
 
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  you = new Player(width/2, height/2);
+// grid exists // current problem is with the display of the grid... [ ]
+class SingleGrid {
+  constructor(rows, cols) {
+    this.rows = rows;
+    this.cols = cols;
+    this.cellSize = 100;
+    this.gridWidth = this.cols*this.cellSize;
+    this.gridHeight = this.rows*this.cellSize;
 
-  //obsolete in future
-  cols = Math.floor(width/CELL_SIZE);
-  rows = Math.floor(height/CELL_SIZE);
+  }
 
-  grid =  generateEmptyGrid(cols, rows);
+  generateGrid() {
+    this.grid = [];
+    for (let y = 0; y < this.rows; y++) {
+      this.grid.push([]);
+      for (let x = 0; x < this.cols; x++) {
+        this.grid[y].push(0);
+      }
+    }
+  }
+
+  displayGrid() {
+    for (let y = 0; y < this.rows; y++) {
+      for (let x = 0; x < this.cols; x++) {
+        fill(200);
+        stroke(180);
+        //rect(x*cellSize + width/2 - cellSize*PASSWORD_LENGTH/2, y*cellSize +100, cellSize, cellSize);
+        rect(x*this.cellSize + width/2 - this.gridWidth/2, y*this.cellSize + this.height/2 - this.gridHeight/2, this.cellSize, this.cellSize);
+      }
+    }
+  }
 }
+
 
 function draw() {
-  background(220);
-  displayGrid();
+  background(60);
 
   if (gameState === "start") {
+    fill(255);
     text("start screen", width/2, height/2);
   }
   else if (gameState === "ongoing") {
+    displayGrid();
+    // gridOne.displayGrid();
     you.move();
     you.display();
   }
@@ -130,6 +183,10 @@ function keyPressed() {
   }
 }
 
+function movePlayerBetweenGrids() {
+
+}
+
 function keyReleased() {
 
   //player Movement
@@ -156,7 +213,8 @@ function displayGrid() {
     for (let x = 0; x < cols; x++) {
       fill(200);
       stroke(180);
-      square(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE);
+      //rect(x*cellSize + width/2 - cellSize*PASSWORD_LENGTH/2, y*cellSize +100, cellSize, cellSize);
+      rect(x*CELL_SIZE + width/2 - gridWidth/2, y*CELL_SIZE + height/2 - gridHeight/2, CELL_SIZE);
     }
   }
 }
