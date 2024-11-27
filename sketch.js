06754279ab655cdb8d0ad339b1321b972d267eb4
solure-ace <av.lynne.w@gmail.com>
2024-11-27 14:15:52 -0600

    Major Project - grid Class display fixed, work on player boundaries

--- sketch.js
+++ sketch.js
@@ -10,9 +10,12 @@ let you;
 
 //make grid into a class because i will need to make lots of them of different sizes.
 // v grid for class
+// i will give hard coded grids actual names (later), random grids wont need names because they will be kept in an array.
 let gridOne; 
 
-//grid that works rn ( not an object)
+let gridTwo;
+
+//random hard coded grid??? yeah
 let grid;
 let cols = 4;
 let rows = 6;
@@ -20,26 +23,32 @@ const CELL_SIZE = 100;
 let gridWidth = cols*CELL_SIZE;
 let gridHeight = rows*CELL_SIZE;
 
+
+let currentGrid = 1;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // cant use you.width and you. height because they dont exist yet...???
-
+  // cant use you.width and you.height because they dont exist yet...???
+  //WAIT
   // you = new Player(gridWidth/2-15, gridHeight/2-40);
 
-  you = new Player(width/2-15, height/2 - 40);
+  you = new Player(width/2, height/2);
 
-  grid =  generateEmptyGrid(cols, rows);
+  // grid =  generateEmptyGrid(cols, rows);
 
-  // gridOne = new SingleGrid(6, 4);
-  // gridOne.generateGrid();
+  gridOne = new SingleGrid(5, 5);
+  gridOne.generateGrid();
 }
 
 class Player {
   constructor(x, y) {
-    this.x = x;
-    this.y = y;
     this.height = 80;
     this.width = 30;
+
+    this.x = x-this.width/2;
+    this.y = y-this.height/2;
+
+    //used to pause player input during dialouge ect.
     this.canMove = true;
     this.speed = 10;
   }
@@ -47,10 +56,19 @@ class Player {
   display() {
     noStroke();
     fill(150, 200, 230);
-    rect(this.x, this.y, this.width, this.height);
+    /////////////////////////////////////////// he pill
+    rect(this.x, this.y, this.width, this.height, 50);
   }
 
   move() {
+
+    // from bouncing ball demo
+    // if (someBall.x > width - someBall.radius || someBall.x < 0 + someBall.radius) {
+    //   someBall.dx *= -1;
+    // }
+    // if (someBall.y > height - someBall.radius || someBall.y < 0 + someBall.radius) {
+    //   someBall.dy *= -1;
+    // }
 
     //           MAIN FOUR DIRECTIONS
     //forwards / north
@@ -73,7 +91,7 @@ class Player {
       this.x -= this.speed;
     }
 
-    //           DIAGNOLS
+    //           DIAGONALS
     // forwards/right / northeast
     if (this.north && !this.south && this.east && !this.west) {
       this.y -= this.speed-sqrt(2)/2;
@@ -99,21 +117,21 @@ class Player {
     }
   }
 
-  // stayWithinGrid() {
-  //   if (this.x < ) {
+  // stayWithinGrid(cols, rows) {
+
+  //   //left wall
+  //   if (this.x < width - cols * CELL_SIZE / 2) {
   //   }
   // }
 }
 
 
-// grid exists // current problem is with the display of the grid... [ ]
 class SingleGrid {
-  constructor(rows, cols) {
+  constructor(cols, rows) {
     this.rows = rows;
     this.cols = cols;
-    this.cellSize = 100;
-    this.gridWidth = this.cols*this.cellSize;
-    this.gridHeight = this.rows*this.cellSize;
+    this.gridWidth = this.cols*CELL_SIZE;
+    this.gridHeight = this.rows*CELL_SIZE;
 
   }
 
@@ -132,8 +150,7 @@ class SingleGrid {
       for (let x = 0; x < this.cols; x++) {
         fill(200);
         stroke(180);
-        //rect(x*cellSize + width/2 - cellSize*PASSWORD_LENGTH/2, y*cellSize +100, cellSize, cellSize);
-        rect(x*this.cellSize + width/2 - this.gridWidth/2, y*this.cellSize + this.height/2 - this.gridHeight/2, this.cellSize, this.cellSize);
+        rect(x*CELL_SIZE + width/2 - this.gridWidth/2, y*CELL_SIZE+ height/2 - this.gridHeight/2, CELL_SIZE, CELL_SIZE);
       }
     }
   }
@@ -148,14 +165,21 @@ function draw() {
     text("start screen", width/2, height/2);
   }
   else if (gameState === "ongoing") {
-    displayGrid();
-    // gridOne.displayGrid();
+
+    // displayGrid();
+
+    displayOneGrid();
+
     you.move();
     you.display();
   }
 }
 
-
+function displayOneGrid() {
+  if (currentGrid === 1) {
+    gridOne.displayGrid();
+  }
+}
 
 
 function mousePressed() {
@@ -183,9 +207,9 @@ function keyPressed() {
   }
 }
 
-function movePlayerBetweenGrids() {
+// function movePlayerBetweenGrids() {
 
-}
+// }
 
 function keyReleased() {
 
@@ -200,7 +224,7 @@ function keyReleased() {
     if (key === "d") {
       you.east = false;
     }
-    if (key === "a") {
+    if (key === "a" ) {
       you.west = false;
     }
   }
